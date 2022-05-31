7bf20e92515d0a62ba3b42ac526ce3278e599f3f
ChienYchieh <91307389+ChienYchieh@users.noreply.github.com>
2022-05-31 12:19:39 +0800

    Update app.py

--- Yu-main/app.py
+++ Yu-main/app.py
@@ -40,10 +40,9 @@ def callback():
     return 'OK'
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage,message1=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get = event.message.text
-    get1 = event.message1.text
 #event.gessage.text接收使用者文字訊息
 
     if get == "求籤":
@@ -61,11 +60,6 @@ def handle_message(event):
         message = TextSendMessage(text = str(result) + "%")
     if get == "0":
         message = TextSendMessage(text = "0%")
-    if('水瓶座' in get1):
-        message1 = ImageSendMessage(
-            original_content_url = 'https://i0.wp.com/guidelinehk.com/wp-content/uploads/2021/02/age-of-aquarius-community.jpg?resize=800%2C445&ssl=1.jpg',
-            preview_image_url = 'https://i0.wp.com/guidelinehk.com/wp-content/uploads/2021/02/age-of-aquarius-community.jpg?resize=800%2C445&ssl=1.jpg'
-        )
     if('水瓶座' in get):
         message = TextSendMessage(text = "水瓶是一個持著瓶子在酒吧的美少年蓋尼米德，他是特洛伊的王子。\n有一天，他替父親看羊時，我在天空經過，一看到他的臉就愛上他了，因為太愛他了，我就變身成一隻老鷹擄走他到奧林匹斯山跟我一起住。")
     if('黑人問號' in get):
@@ -77,7 +71,7 @@ def handle_message(event):
     if get == "hello 你好嗎":    
         message = TextSendMessage(text = "衷心感謝")
         
-    line_bot_api.reply_message(event.reply_token, message,message1)
+    line_bot_api.reply_message(event.reply_token, message)
 
 if __name__ == "__main__":
     app.run()
